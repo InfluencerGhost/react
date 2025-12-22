0942db26b8d5c796729d047324b16a984a178b9a
InfluencerGhost <eokorie1911@gmail.com>
2025-12-22 15:24:07 +0100

    onchange practice

--- onchangeEventHan/onchange-app/src/MyComponent.jsx
+++ onchangeEventHan/onchange-app/src/MyComponent.jsx
@@ -1,44 +1,52 @@
 import React from "react";
 
 export default function MyComponent() {
-
-  const [name, setName] = React.useState();
+  const [name, setName] = React.useState("Guest");
   const [quantity, setQuantity] = React.useState(1);
   const [comment, setComment] = React.useState("");
-  const [paymentMethod, setPaymentMethod] = React.useState("");
+  const [paymentMethod, setPaymentMethod] = React.useState("creditCard");
+  const [shipping, setShipping] = React.useState("Delivery");
 
   function changeName(e) {
     setName(e.target.value);
-
   }
 
   function changeQuantity(e) {
     setQuantity(e.target.value);
   }
 
-function changeComment(e) {
+  function changeComment(e) {
     setComment(e.target.value);
   }
 
-function changePaymentMethod(e) {
+  function changePaymentMethod(e) {
     setPaymentMethod(e.target.value);
-  } 
+  }
 
-
+  function changeShipping(e) {
+    setShipping(e.target.value);
+  }
 
   return (
-
     <div>
-    <label>Name: </label>
+      <label>Name: </label>
       <input value={name} onChange={(e) => changeName(e)} />
       <p>Hello, {name}!</p>
 
-    <label>Quantity: </label>
-      <input type="number" value={quantity} onChange={(e) => changeQuantity(e)} />
+      <label>Quantity: </label>
+      <input
+        type="number"
+        value={quantity}
+        onChange={(e) => changeQuantity(e)}
+      />
       <p>You have {quantity} items.</p>
 
       <label>Comment: </label>
-      <textarea placeholder="Enter your comment here" value={comment} onChange={(e) => changeComment(e)} />
+      <textarea
+        placeholder="Enter your comment here"
+        value={comment}
+        onChange={(e) => changeComment(e)}
+      />
       <p>Your comment: {comment}</p>
 
       <select value={paymentMethod} onChange={(e) => changePaymentMethod(e)}>
@@ -49,7 +57,29 @@ function changePaymentMethod(e) {
       </select>
       <p>Selected Payment Method: {paymentMethod}</p>
 
+      <label>
+        {" "}
+        <input
+          type="radio"
+          value="Delivery"
+          checked={shipping === "Delivery"}
+          onChange={(e) => changeShipping(e)}
+        />{" "}
+        Delivery
+      </label>
+
+      <label>
+        {" "}
+        <input
+          type="radio"
+          value="Pick up"
+          checked={shipping === "Pick up"}
+          onChange={(e) => changeShipping(e)}
+        />{" "}
+        Pickup
+      </label>
+
+      <p>Shipping Method: {shipping}</p>
     </div>
   );
-
 }
