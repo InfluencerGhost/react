15ee8273674cf52f8efe31f5de0a9ccec31cd2b7
InfluencerGhost <eokorie1911@gmail.com>
2025-12-23 12:01:31 +0100

    colour picker

---
+++ colourPicker/colour-picker-app/src/App.jsx
@@ -0,0 +1,11 @@
+import ColorPicker from "./ColorPicker.Jsx";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Colour Picker App</h1>
+      <ColorPicker />
+      
+    </div>
+  );
+}
---
+++ colourPicker/colour-picker-app/src/ColorPicker.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+export default function ColorPicker() {
+  const [color, setColor] = React.useState("#FFFFFF");
+  function changeColor(e) {
+    setColor(e.target.value);
+  }
+
+  return (
+    <div className="color-picker-container">
+      <p
+        className="color-display"
+        style={{
+          backgroundColor: color,
+          color:
+            parseInt(color.slice(1), 16) > 0x808080 ? "#000000" : "#FFFFFF",
+        }}
+      >
+        You selected {color.toUpperCase()}
+      </p>
+      <h2>Select Your Color</h2>
+      <input
+        className="color-picker"
+        type="color"
+        value={color}
+        onChange={changeColor}
+      />
+    </div>
+  );
+}
